5d3be266cd213b38790d8070c03d0a16c1b6096f
MeganHancock <mgnhnck@gmail.com>
2024-02-17 21:57:24 -0700

    funtional clock

---
+++ app/controllers/ClockController.js
@@ -0,0 +1,18 @@
+import { setText } from "../utils/Writer.js";
+
+export class ClockController {
+
+    constructor() {
+        this.newDate()
+        setInterval(this.newDate, 1000);
+    }
+
+    newDate() {
+        const newClock = new Date()
+        console.log(newClock.toLocaleTimeString())
+        setText('clock', newClock.toLocaleTimeString())
+    }
+
+
+
+}
--- app/router-config.js
+++ app/router-config.js
@@ -1,5 +1,6 @@
 import { AccountController } from "./controllers/AccountController.js";
 import { BackgroundImageController } from "./controllers/BackgroundImageController.js";
+import { ClockController } from "./controllers/ClockController.js";
 import { HomeController } from "./controllers/HomeController.js";
 import { TodoController } from "./controllers/TodoController.js";
 import { WeatherController } from "./controllers/WeatherController.js";
@@ -10,7 +11,7 @@ import { Router } from "./utils/Router.js";
 export const router = new Router([
   {
     path: '',
-    controllers: [TodoController, WeatherController, BackgroundImageController],
+    controllers: [TodoController, WeatherController, BackgroundImageController, ClockController],
     view: 'app/views/HomeView.html'
   },
 
